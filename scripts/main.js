728256247ad7a56c3b7397b4a06a0e188a8fb0d9
Hassan Salex <7166704@isd535.org>
2022-05-12 11:25:56 -0500

    Commit #1

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,33 @@
 // Put your JavaScript here
+
+let config = {
+    type: Phaser.AUTO,
+    sence: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.game(config);
+var canvas = document.querySelector("game")
+canvas = game
+
+function preload() {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude',
+    'assets/dude.png',
+    { frameWidth: 32, frameHeight: 48 }
+    );
+
+}
+
+function create() {
+    this.add.image(1300, 720, 'sky')
+}
+
+function update() {
+}
